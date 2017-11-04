cff9de70186987ec8891ef8e8d3468f21a27bdd6
Vinod Kumar L <vinod@Vinods-MacBook-Pro.local>
2017-11-04 01:59:34 -0400

    Reorganized code for containment hash

--- build/build.sh
+++ build/build.sh
@@ -1,1 +1,2 @@
-g++ -g -Wall -lboost_system ../src/main.cpp -o ../bin/long_read
+clang++ -g -Wall -std=c++11 ../src/min_hash.cpp ../utils/hash.cpp ../utils/index.cpp -o ../bin/min_hash
+#g++ -g -Wall -lboost_system ../src/containment_hash.cpp -o ../bin/containment_hash
---
+++ include/common.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#import <vector>
+#import <string>
+
+using namespace std;
+
+typedef unsigned int uint;
